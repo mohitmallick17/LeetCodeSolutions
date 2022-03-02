f6c21538624b48a8d5054e40daa804758a403f37
Mohit Mallick <mohitmallick17@gmail.com>
2022-03-02 21:50:11 +0530

    Time: 366 ms (53.30%), Space: 8.7 MB (96.59%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -12,23 +12,24 @@ public:
     int findMaxForm(vector<string>& strs, int m, int n) {
         int len=strs.size();
 
-        int dp[len+1][m+1][n+1];
+        int dp[2][m+1][n+1];
         memset(dp, 0, sizeof(dp));
-        
+        bool flag = true;
         for(int i=1;i<=len;i++){
+            flag = !flag;
             for(int j=0;j<=m;j++){
                 for(int k=0;k<=n;k++){
                     pair<int, int> p = getDigitCount(strs[i-1]);
                     int zeros=p.first;
                     int ones=p.second;
                     
-                    int res = dp[i-1][j][k];
+                    int res = dp[1-flag][j][k];
                     if(zeros <= j and ones <= k)
-                        res = max(res, dp[i-1][j-zeros][k-ones] + 1);
-                    dp[i][j][k] = res;
+                        res = max(res, dp[1-flag][j-zeros][k-ones] + 1);
+                    dp[flag][j][k] = res;
                 }
             }
         }
-        return dp[len][m][n];
+        return dp[flag][m][n];
     }
 };
