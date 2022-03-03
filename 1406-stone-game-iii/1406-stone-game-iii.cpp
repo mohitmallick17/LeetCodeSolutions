9aaa551640e098b4f68f1b1d2c64b6acb2500d23
Mohit Mallick <mohitmallick17@gmail.com>
2022-03-04 01:50:51 +0530

    Time: 264 ms (77.48%), Space: 126.4 MB (91.06%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -2,21 +2,23 @@ class Solution {
 public:
     string stoneGameIII(vector<int>& v) {
         int n=v.size();
-        vector<int> dp(n+1,0);
+        int next1=0, next2=0, next3=0;
         int i=n-1;
         
         while(i>=0){
-            int ans = v[i] - dp[i+1];
+            int ans = v[i] - next1;
             if(i+1 < n)
-               ans = max(ans,  v[i]+v[i+1]-dp[i+2]);
+               ans = max(ans,  v[i]+v[i+1]-next2);
             if(i+2 < n)
-            ans = max(ans, v[i]+v[i+1]+v[i+2]-dp[i+3]);
-
-            dp[i]=ans;
+            ans = max(ans, v[i]+v[i+1]+v[i+2]-next3);
+            
+            next3=next2;
+            next2=next1;
+            next1=ans;
             i--;
         }
         
-        int res = dp[0];
+        int res = next1;
         if(res==0)return "Tie";
         return (res > 0) ? "Alice" : "Bob";
     }
