85df58e6657e5f6c6731b6242858781159f2a808
Mohit Mallick <mohitmallick17@gmail.com>
2022-03-03 18:57:33 +0530

    Time: 0 ms (100.00%), Space: 6 MB (65.03%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -1,22 +1,17 @@
 class Solution {
-    int INF = (1 << 30);
-    vector<vector<int>> dp;
-    set<pair<int, int>> vis;
-    int solve(int screen, int clipboard, int n){
-        if(screen == n)
-            return 0;
-        if(screen > n or vis.find({screen, clipboard})!=vis.end())
-            return INF;
-        vis.insert({screen, clipboard});
-        if(dp[screen][clipboard] != -1)return dp[screen][clipboard];
-        int copy = 1 + solve(screen, screen, n);
-        int paste = (clipboard > 0 ) ? 1 + solve(screen + clipboard, clipboard, n) : INF; // paste
-        
-        return dp[screen][clipboard] = min(copy, paste);
-    }
 public:
     int minSteps(int n) {
-        dp.resize(n+1, vector<int>(n+1, -1));
-        return solve(1, 0, n);
+        long long ans = 0;
+        
+        for(long long i=2;i*i <= (long long)n;){
+            if(n % i == 0){
+                ans += i;
+                n /= i;
+            }else
+                i++;
+        }
+        if(n > 1)
+            ans += n;
+        return ans;
     }
 };
