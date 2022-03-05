820303a230c5bd666dfbe30d42d48799f8c6f6ed
Mohit Mallick <mohitmallick17@gmail.com>
2022-03-05 22:37:56 +0530

    Time: 866 ms (14.29%), Space: 102 MB (28.57%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
@@ -1,26 +1,22 @@
 
 class Solution {
-    // bool cmp(vector<int> a, vector<int>b){
-    //     // if(a[0]==b[0])
-    //     return a[0] < b[0];
-    // }
 public:
     vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
-        vector<vector<int>> v;
         int n=nums.size();
+        
+        vector<vector<int>> v(n);
         for(int i=0;i<n;i++){
             string a = to_string(nums[i]);
             string b;
-            for(char &c:a){
+            for(char &c:a)
                 b += '0' + mapping[c-'0'];
-            }
-            v.push_back({stoi(b), i});
+            v[i] = {stoi(b), i};
         }
         sort(v.begin(), v.end());
         vector<int> ans(n);
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++)
             ans[i] = nums[v[i][1]];
-        }
+        
         return ans;
     }
 };
