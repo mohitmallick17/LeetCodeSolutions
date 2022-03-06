2a37ca6be38cccbcab38f9f1d5a1d23f13e7372f
Mohit Mallick <mohitmallick17@gmail.com>
2022-03-06 23:32:04 +0530

    Time: 0 ms (100.00%), Space: 6 MB (31.45%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,22 +1,11 @@
 class Solution {
-    vector<vector<long long>> dp;
-    long long mod = 1e9 + 7;
-    long long solve(int p, int d){
-        if(p==0 and d==0){
-            return 1LL;
-        }
-        if(dp[p][d] != -1LL)return dp[p][d];
-        long long ans = 0LL;
-        if(p > 0)
-            ans = (((solve(p-1, d+1))%mod)*p)%mod;
-        if(d > 0)
-            ans = (ans + (((solve(p, d-1)%mod)*d)%mod))%mod;
-        
-        return dp[p][d] = ans;
-    }
 public:
     int countOrders(int n) {
-        dp.resize(n+1, vector<long long>(n+1, -1LL));
-        return solve(n, 0);
+        long long ans = 1;
+        long long mod = 1e9 + 7;
+        for(int i=1;i<=n;i++){
+            ans = (((ans*i)%mod) * (2*i-1))%mod;
+        }
+        return ans;
     }
 };
