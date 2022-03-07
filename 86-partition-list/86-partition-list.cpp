c9d115f6182512c6601bcfb02255ce5e606159cb
Mohit Mallick <mohitmallick17@gmail.com>
2022-03-07 18:32:27 +0530

    Time: 8 ms (55.07%), Space: 10.3 MB (31.89%) - LeetHub

--- 86-partition-list/86-partition-list.cpp
+++ 86-partition-list/86-partition-list.cpp
@@ -12,35 +12,26 @@ class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
         if(!head)return head;
-        ListNode* d1 = new ListNode(-1), *d3=new ListNode(-1); // , *d2=new ListNode(-1), 
+        ListNode* d1 = new ListNode(-1), *d2=new ListNode(-1);
         
-        ListNode* r1=d1, *r3=d3; //*r2=d2, 
+        ListNode* r1=d1, *r2=d2;
         ListNode* cur = head;
         while(cur){
             if(cur->val < x){
                 r1->next=cur;
                 r1=r1->next;
             }
-            // else if(cur->val == x){
-            //     r2->next=cur;
-            //     r2 = r2->next;
-            // }
             else{
-                r3->next=cur;
-                r3=r3->next;
+                r2->next=cur;
+                r2=r2->next;
             }
             cur=cur->next;
         }
-        // if(d2->next){
-        //     r3->next=NULL; // end of list
-        //     r2->next = d3->next;
-        // }else
-        //     d2=d3;
-        r3->next = NULL;
+        r2->next = NULL;
         if(d1->next){
-            r1->next = d3->next;
+            r1->next = d2->next;
         }else
-            d1=d3;
+            d1=d2;
         return d1->next;
     }
 };
