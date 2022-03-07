3926d2a9ce4ace1aa2299e11f0a5d1554416a999
Mohit Mallick <mohitmallick17@gmail.com>
2022-03-07 18:12:25 +0530

    Time: 128 ms (91.24%), Space: 83.3 MB (76.15%) - LeetHub

--- 775-global-and-local-inversions/775-global-and-local-inversions.cpp
+++ 775-global-and-local-inversions/775-global-and-local-inversions.cpp
@@ -1,55 +1,17 @@
 class Solution {
-    
-    long long merge(long long low, long long mid, long long high, vector<int>&a, vector<int>&tmp){
-        long long i=low, j=mid, k=low, count=0;
-        
-        while(i<mid and j<=high){
-            if(a[i] <= a[j])
-                tmp[k++] = a[i++];
-            else{
-                tmp[k++] = a[j++];
-                count += mid-i;
-            }
-        }
-        while(i < mid)
-            tmp[k++] = a[i++];
-        while(j <= high)
-            tmp[k++] = a[j++];
-        for(long long x=low;x<=high;x++)
-            a[x]=tmp[x];
-        return count;
-    }
-    long long mergeSort(long long low, long long high, vector<int>&a, vector<int>&tmp){
-        long long mid, count = 0LL;
-        if(low < high){
-            mid = low + (high-low)/2;
-            count += mergeSort(low, mid, a, tmp);
-            count += mergeSort(mid+1, high, a, tmp);
-            count += merge(low, mid+1, high, a, tmp);
-        }
-        return count;
-    }
-  public:
-    // arr[]: Input Array
-    // N : Size of the Array arr[]
-    // Function to count inversions in the array.
-    long long int inversionCount(vector<int>&a){
-        vector<int> tmp(a.size());
-        return mergeSort(0, a.size()-1, a, tmp);
-    }
-    
-    int getLocals(vector<int>& a){
-        int cnt=0;
-        for(int i=0;i<a.size()-1;i++){
-            if(a[i] > a[i+1])cnt++;
-        }
-        return cnt;
-    }
 public:
     bool isIdealPermutation(vector<int>& nums) {
-        int locals = getLocals(nums);
-        int globals = inversionCount(nums);
+        // Always, locals <= globals
+        // every local is also a global
+        // if we find 1 global which is not zero -> return false
+        // i.e i < j where j >= i+2 and j < n. for all i
         
-        return globals == locals;
+        int maxVal = -1;
+        for(int i=0;i<(int)nums.size()-2;i++){
+            maxVal = max(maxVal, nums[i]); // maximize the value
+            // if any number at j (j>=i+2) is less than any previous i, it's a global (with gap >= 2)
+            if(maxVal > nums[i+2])return false;
+        }
+        return true;
     }
 };
