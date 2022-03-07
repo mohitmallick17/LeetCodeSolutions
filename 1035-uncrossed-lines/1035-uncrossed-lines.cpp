743e2ccf622060b7090ababffd2e44e092d25b06
Mohit Mallick <mohitmallick17@gmail.com>
2022-03-07 21:13:17 +0530

    Time: 24 ms (44.29%), Space: 13.1 MB (30.77%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -1,28 +1,16 @@
 class Solution {
-    int solve(int i, int limit, vector<int>& nums2, unordered_map<int, vector<int>> &nums1
-              ,vector<vector<int>> &dp) {
-        if(i >= nums2.size())return 0;
+    int solve(int i, int j, vector<int>& nums1, vector<int>& nums2, vector<vector<int>> &dp) {
+        if(i == nums1.size() or j == nums2.size())return 0;
         
-        if(dp[i][limit] != -1)return dp[i][limit];
-        if(nums1.find(nums2[i]) == nums1.end())
-            return dp[i][limit]=solve(i+1, limit, nums2, nums1, dp);
-        
-        // exclude ith num
-        int res = solve(i+1, limit, nums2, nums1, dp);
-        
-        for(int val:nums1[nums2[i]]){
-            if(val >= limit)
-                res = max(res, 1 + solve(i+1, val+1, nums2, nums1, dp));
-        }
-        return dp[i][limit]=res;
+        if(dp[i][j] != -1)return dp[i][j];
+        if(nums1[i] == nums2[j])
+            return dp[i][j] = 1 + solve(i+1, j+1, nums1, nums2, dp);
+        else
+            return dp[i][j] = max(solve(i, j+1, nums1, nums2, dp), solve(i+1, j, nums1, nums2, dp));
     }
 public:
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, vector<int>> mp;
-        for(int i=0;i<nums1.size();i++){
-            mp[nums1[i]].push_back(i);
-        }
-        vector<vector<int>> dp(nums2.size(), vector<int>(nums1.size()+1, -1));
-        return solve(0, 0, nums2, mp, dp);
+        vector<vector<int>> dp(nums1.size(), vector<int>(nums2.size(), -1));
+        return solve(0, 0, nums1, nums2, dp);
     }
 };
