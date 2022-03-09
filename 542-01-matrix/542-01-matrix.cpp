ab687ce33f1d0e62dde3e8c3876b99e9538ed0b1
Mohit Mallick <mohitmallick17@gmail.com>
2022-03-09 18:28:48 +0530

    Time: 92 ms (67.40%), Space: 30.1 MB (61.17%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -2,36 +2,29 @@ class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int m=mat.size(), n=mat[0].size();
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
         queue<pair<int, int>> q;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(mat[i][j]==0){
+                if(mat[i][j]==0)
                     q.push({i, j});
-                    visited[i][j]=true;
-                }        
+                else
+                    mat[i][j] = -1;
             }
         }
-        vector<vector<int>> dirs{{0,1}, {0,-1}, {1,0}, {-1, 0}};
+        vector<int> dirs{0, 1, 0, -1, 0};
         
-        int dist = 0;
         while(!q.empty()){
-            int size=q.size();
-            while(size--){
-                int i = q.front().first;
-                int j = q.front().second;
-                q.pop();
-                mat[i][j]=dist;
-                for(auto &dir:dirs){
-                    int x=dir[0]+i;
-                    int y=dir[1]+j;
-                    if(x>=0 and y>=0 and x<m and y<n and !visited[x][y]){
-                        visited[x][y]=true;
-                        q.push({x, y});
-                    }
+            int i = q.front().first;
+            int j = q.front().second;
+            q.pop();
+            for(int d=0;d<4;d++){
+                int x=dirs[d]+i;
+                int y=dirs[d+1]+j;
+                if(x>=0 and y>=0 and x<m and y<n and mat[x][y]==-1){
+                    mat[x][y] = mat[i][j] + 1;
+                    q.push({x, y});
                 }
             }
-            dist++;
         }
         return mat;
     }
