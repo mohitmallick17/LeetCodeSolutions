20cbdaeb1fce14660d102405a63347a528f7bb38
Mohit Mallick <mohitmallick17@gmail.com>
2022-03-09 21:19:16 +0530

    Time: 101 ms (58.90%), Space: 27.4 MB (95.02%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -2,28 +2,24 @@ class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int m=mat.size(), n=mat[0].size();
-        queue<pair<int, int>> q;
+        int INF = m+n;
+        
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(mat[i][j]==0)
-                    q.push({i, j});
-                else
-                    mat[i][j] = -1;
+                if(mat[i][j] == 0)continue;
+                int up = INF, left = INF;
+                if(i-1 >= 0)up=mat[i-1][j];
+                if(j-1 >= 0)left=mat[i][j-1];
+                mat[i][j] = min(up, left) + 1;
             }
         }
-        vector<int> dirs{0, 1, 0, -1, 0};
-        
-        while(!q.empty()){
-            int i = q.front().first;
-            int j = q.front().second;
-            q.pop();
-            for(int d=0;d<4;d++){
-                int x=dirs[d]+i;
-                int y=dirs[d+1]+j;
-                if(x>=0 and y>=0 and x<m and y<n and mat[x][y]==-1){
-                    mat[x][y] = mat[i][j] + 1;
-                    q.push({x, y});
-                }
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(mat[i][j] == 0)continue;
+                int down = INF, right = INF;
+                if(i+1 < m)down=mat[i+1][j];
+                if(j+1 < n)right=mat[i][j+1];
+                mat[i][j] = min(mat[i][j], min(down, right) + 1);
             }
         }
         return mat;
