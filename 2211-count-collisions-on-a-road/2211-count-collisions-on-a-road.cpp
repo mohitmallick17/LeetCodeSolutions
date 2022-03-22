12b1fc505d527cce6303d6a207a96eab5852b1fb
Mohit Mallick <mohitmallick17@gmail.com>
2022-03-23 01:43:08 +0530

    Time: 95 ms (38.55%), Space: 16.2 MB (98.28%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     int countCollisions(string directions) {
-        vector<pair<char, int>> v;
         int n=directions.size();
-        for(int i=0;i<n;i++){
-            int cnt=1;
-            while(i+1<n and directions[i]==directions[i+1])
-                i++, cnt++;
-            v.push_back({directions[i], cnt});
+        int left=0, right=n-1;
+        while(left < n and directions[left]=='L')left++;
+        while(right >= 0 and directions[right]=='R')right--;
+        
+        int count=0;
+        for(int i=left;i<=right;i++){
+            if(directions[i] != 'S')count++;
         }
-        int cnt = 0;
-        for(int i=0;i<v.size()-1;i++){
-            if(v[i].first == 'R' and v[i+1].first == 'L')
-                cnt += v[i].second + v[i+1].second;
-            else if(v[i].first == 'R' and v[i+1].first == 'S')
-                cnt += v[i].second;
-            else if(v[i].first == 'S' and v[i+1].first == 'L')
-                cnt += v[i+1].second;
-        }
-        return cnt;
+        return count;
     }
 };
