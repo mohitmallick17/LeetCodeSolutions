03085a55bacf1d067e6f37b42598e1315708c7a2
Mohit Mallick <mohitmallick17@gmail.com>
2022-03-24 03:52:35 +0530

    Time: 409 ms (27.39%), Space: 48.9 MB (11.19%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
@@ -1,8 +1,11 @@
 class Solution {
     bool isIntersecting(vector<int>& a, vector<int>& b){
+        // (x2-x1)^2 + (y2-y1)^2
         long long distsq = 1LL*(a[0] - b[0])*(a[0] - b[0]) + 1LL*(a[1] - b[1])*(a[1] - b[1]);
+        // a=src, so radius = src_radius^2
         long long rdist = 1LL*a[2]*a[2];
-        if(distsq > rdist)
+        
+        if(distsq > rdist) // non intersecting
             return false;
         return true;
     }
@@ -10,13 +13,11 @@ class Solution {
     void buildGraph(vector<vector<int>>& bombs, map<int, vector<int>> &graph){
         int n=bombs.size();
         for(int i=0;i<n;i++){
-            vector<int> nbrs;    
             for(int j=0;j<n;j++){
                 if(i==j)continue;
                 if(isIntersecting(bombs[i], bombs[j]))
-                    nbrs.push_back(j);
+                    graph[i].push_back(j);
             }
-            graph[i] = nbrs;
         }
     }
     int bfs(int src, map<int, vector<int>> &graph){
