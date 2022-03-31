ba03a3f1ed87b6f462bfd51ee4dfd19bdf200504
Mohit Mallick <mohitmallick17@gmail.com>
2022-03-31 19:17:53 +0530

    Time: 12 ms (19.20%), Space: 8.2 MB (21.96%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     int twoCitySchedCost(vector<vector<int>>& costs) {
-        vector<int> refund;
-        int N = costs.size()/2;
-        int minCost = 0;
-        for(vector<int> cost : costs){
-            minCost += cost[0];
-            refund.push_back(cost[1] - cost[0]); 
+        int n=costs.size();
+        vector<pair<int, int>> v(n);
+        for(int i=0;i<costs.size();i++){
+            v[i] = {costs[i][1]-costs[i][0], i};
         }
-        sort(refund.begin(), refund.end());
-        for(int i = 0; i < N; i++){
-            minCost += refund[i];
+        sort(v.begin(), v.end()); // sort by difference in increasing order
+        int res = 0;
+        
+        for(int i=0;i<n;i++){
+            if(i < n/2){
+                // choose B
+                // difference of A and B is small, pick B 
+                res += costs[v[i].second][1];
+            }else{
+                // choose A
+                // difference of A and B is large, pick A '.' B > A
+                res += costs[v[i].second][0];
+            }
         }
-        return minCost;
+        return res;
     }
 };
