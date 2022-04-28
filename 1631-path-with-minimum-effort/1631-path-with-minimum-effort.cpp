222ffbab0454af01ff93fb411211ed99a92817cc
Mohit Mallick <mohitmallick17@gmail.com>
2022-04-29 01:56:45 +0530

    Time: 188 ms (82.22%), Space: 20.1 MB (82.22%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,3 +1,9 @@
+class cmp{
+public:
+    bool operator()(pair<int, pair<int, int>> &a, pair<int, pair<int, int>> &b){
+        return a.first > b.first;
+    }
+};
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
@@ -5,33 +11,33 @@ public:
         vector<vector<int>> c(m, vector<int>(n, INT_MAX));
         c[0][0]=0;
         
-        queue<pair<int, int>> q;
-        q.push({0, 0});
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, cmp> q;
+        q.push({0, {0, 0}});
         
         while(!q.empty()){
-            auto front=q.front();
+            auto front=q.top();
             q.pop();
-            int i=front.first, j=front.second;
+            int i=front.second.first, j=front.second.second;
             
             if(i==m-1 and j==n-1)continue;
             
             if(i+1 < m and c[i+1][j] > max(c[i][j],abs(heights[i+1][j]-heights[i][j]))){
                 c[i+1][j] = max(c[i][j],abs(heights[i+1][j]-heights[i][j]));
-                q.push({i+1, j});
+                q.push({c[i+1][j], {i+1, j}});
             }
             if(j+1 < n and c[i][j+1] > max(c[i][j],abs(heights[i][j+1]-heights[i][j]))){
                 c[i][j+1] = max(c[i][j],abs(heights[i][j+1]-heights[i][j]));
-                q.push({i, j+1});
+                q.push({c[i][j+1], {i, j+1}});
             }
             
             
             if(i-1 >= 0 and c[i-1][j] > max(c[i][j],abs(heights[i-1][j]-heights[i][j]))){
                 c[i-1][j] = max(c[i][j],abs(heights[i-1][j]-heights[i][j]));
-                q.push({i-1, j});
+                q.push({c[i-1][j], {i-1, j}});
             }
             if(j-1 >= 0 and c[i][j-1] > max(c[i][j],abs(heights[i][j-1]-heights[i][j]))){
                 c[i][j-1] = max(c[i][j],abs(heights[i][j-1]-heights[i][j]));
-                q.push({i, j-1});
+                q.push({c[i][j-1], {i, j-1}});
             }
             
         }
