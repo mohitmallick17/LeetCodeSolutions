286dc43b12877f0275741db0af47744e4dcb82cb
Mohit Mallick <mohitmallick17@gmail.com>
2022-05-24 10:23:59 +0530

    Time: 531 ms (30.12%), Space: 35.9 MB (40.01%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,35 +1,33 @@
 class Solution {
-    pair<int, int> getDigitCount(string &s){
+    int dp[601][101][101];
+    pair<int, int> getCnt(string &s){
         int z=0, o=0;
-        for(char& c:s){
+        for(char &c:s){
             if(c=='0')z++;
             else o++;
         }
-        return {z,o};
+        return {z, o};
     }
-    
+    int solve(int i, vector<pair<int, int>> &cnt, int m, int n){
+        if(i==cnt.size())return 0;
+        if(dp[i][m][n] != -1)return dp[i][m][n];
+        int res;
+        
+        if(m >= cnt[i].first and n >= cnt[i].second){
+            // can include this set, as (m, n) > set(cnt[i])
+            // choice: include, skip
+            res = max(solve(i+1, cnt, m, n), 1+solve(i+1, cnt, m-cnt[i].first, n-cnt[i].second));
+        }else
+            res = solve(i+1, cnt, m, n);
+        
+        return dp[i][m][n]=res;
+    }
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
-        int len=strs.size();
-
-        int dp[2][m+1][n+1];
-        memset(dp, 0, sizeof(dp));
-        bool flag = true;
-        for(int i=1;i<=len;i++){
-            flag = !flag;
-            for(int j=0;j<=m;j++){
-                for(int k=0;k<=n;k++){
-                    pair<int, int> p = getDigitCount(strs[i-1]);
-                    int zeros=p.first;
-                    int ones=p.second;
-                    
-                    int res = dp[1-flag][j][k];
-                    if(zeros <= j and ones <= k)
-                        res = max(res, dp[1-flag][j-zeros][k-ones] + 1);
-                    dp[flag][j][k] = res;
-                }
-            }
-        }
-        return dp[flag][m][n];
+        vector<pair<int, int>> cnt(strs.size());
+        for(int i=0;i<strs.size();i++)
+            cnt[i] = getCnt(strs[i]);   
+        memset(dp, -1, sizeof(dp));
+        return solve(0, cnt, m, n);
     }
 };
