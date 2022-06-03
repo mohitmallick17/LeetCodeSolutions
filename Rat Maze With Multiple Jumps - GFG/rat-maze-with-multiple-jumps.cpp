f165de0cb085ec5fd8cb24a27322f771bf699326
Mohit Mallick <mohitmallick17@gmail.com>
2022-06-03 16:46:02 +0530

    Added solution - LeetHub

--- Rat Maze With Multiple Jumps - GFG/rat-maze-with-multiple-jumps.cpp
+++ Rat Maze With Multiple Jumps - GFG/rat-maze-with-multiple-jumps.cpp
@@ -4,72 +4,31 @@ using namespace std;
 
  // } Driver Code Ends
 class Solution {
-//     int solve(int i, int j, vector<vector<int>> &g, vector<vector<int>> &res, int m, int n){
-//         if(i>=m or j>=n or or g[i][j]==0)return INT_MAX;
+    bool solve(int i, int j, vector<vector<int>>&g, vector<vector<int>>&ans){
+        int n=g.size();
         
-//         res[i][j]=1;
+        if(i>=n or j>=n)return false;
+        ans[i][j]=1;
+        if(i==n-1 and j==n-1)return true;
         
-//         bool found=false;
-//         int cost=INT_MAX;
-        
-//         for(int x=i;x<min(i+g[i][j], m);x++){
-//             for(int y=i;x<min(j+g[i][j], n);y++){
-//                 int val = solve(x, y, g, res, m,n );
-//                 if(val != INT_MAX){
-//                     found=true;
-//                     cost = min(cost, val);
-//                 }
-//             }    
-//         }
-//         if(!found){
-//             res[i][j]=0;
-//             return INT_MAX;
-//         }
-//         return cost+1;
-//     }
-// public:
-//     bool isSafe(vector<vector<int>> &matrix, int x, int y){
-//         if(x>=matrix.size() || y>=matrix[0].size() || matrix[x][y]==0){
-//             return false;
-//         }
-//         return true;
-// }
-public:
-bool isSafe(vector<vector<int>> &matrix, int x, int y){
-        if(x>=matrix.size() || y>=matrix[0].size() || matrix[x][y]==0){
-            return false;
+        for(int jumps=1;jumps<=g[i][j];jumps++){
+            // forward
+            if(solve(i, j+jumps, g, ans))return true;
+            // downwards
+            if(solve(i+jumps, j, g, ans))return true;
         }
-        return true;
-}
-bool helper(vector<vector<int>> &matrix, vector<vector<int>> &res, int x, int y, int n){
-        if(x==n-1 && y==n-1){
-            res[x][y]=1;
-            return true;
-        }
-        if(isSafe(matrix,x,y)==true){
-            res[x][y]=1;
-            for(int i=1;i<=matrix[x][y] && i<n;i++){
-                if(helper(matrix,res,x,y+i,n)==true){
-                    return true;
-                }
-                if(helper(matrix,res,x+i,y,n)==true){
-                    return true;
-                }
-            }
-            res[x][y]=0;
-            return false;
-        }
+        // backtrack
+        ans[i][j]=0;
         return false;
-}
-vector<vector<int>> ShortestDistance(vector<vector<int>> &matrix){
-	    int n=matrix.size();
-	    if(matrix[0][0]==0 && n!=1){
-	        return {{-1}};
-	    }
-	    vector<vector<int>> res(n,vector<int>(n,0));
-	    helper(matrix,res,0,0,n);
-	    return res;
-}
+    }
+    
+public:
+	vector<vector<int>> ShortestDistance(vector<vector<int>>&g){
+	   int n=g.size();
+	   vector<vector<int>> ans(n, vector<int>(n, 0));
+	   if(solve(0,0,g,ans))return ans;
+	   return {{-1}};
+	}
 
 };
 
