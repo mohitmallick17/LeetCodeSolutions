7009e6d5ba44799135f8e97814ef2c5fc2e38324
Mohit Mallick <mohitmallick17@gmail.com>
2022-06-04 20:54:45 +0530

    Time: 51 ms (83.49%), Space: 16 MB (51.36%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,20 +1,21 @@
 class Solution {
-public:
-    int dp[200][200]{}; // constraints are small enough that we can just set them to MAX
-    int maxPath, n, m;
-    int longestIncreasingPath(vector<vector<int>>& matrix) {
-        maxPath = 0, n = size(matrix), m = size(matrix[0]);
-        for(int i = 0; i < n; i++)
-            for(int j = 0; j < m; j++)
-                maxPath = max(maxPath, solve(matrix, i, j, -1));            
-        return maxPath;
+    int solve(int i, int j, vector<vector<int>> &dp, vector<vector<int>> &g, int prev){
+        if(i<0 or j<0 or i>=g.size() or j>=g[0].size() or prev >= g[i][j])return 0;
+        // cout << i << ' ' << j << '\n';
+        if(dp[i][j] != 0)return dp[i][j];
+        
+        return dp[i][j] = 1 + max({solve(i-1,j,dp,g,g[i][j]),solve(i+1,j,dp,g,g[i][j]),solve(i,j-1,dp,g,g[i][j]),solve(i,j+1,dp,g,g[i][j])});
     }
-    int solve(vector<vector<int>>& mat, int i, int j, int prev){
-        if(i < 0 || j < 0 || i >= n || j >= m || mat[i][j] <= prev) return 0;
-        if(dp[i][j]) return dp[i][j];
-        return dp[i][j] = 1 + max({ solve(mat, i + 1, j, mat[i][j]),
-                                    solve(mat, i - 1, j, mat[i][j]),
-                                    solve(mat, i, j + 1, mat[i][j]),
-                                    solve(mat, i, j - 1, mat[i][j]) });       
+public:
+    int longestIncreasingPath(vector<vector<int>>& matrix){
+        int m=matrix.size(), n=matrix[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        int res=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                res = max(res, solve(i,j,dp,matrix,-1));
+            }
+        }
+        return res;
     }
 };
