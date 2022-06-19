4a038f17b5a9e88054c52f12eac79935eac3f294
Mohit Mallick <mohitmallick17@gmail.com>
2022-06-19 17:03:29 +0530

    Time: 155 ms (100.00%), Space: 24.8 MB (100.00%) - LeetHub

--- 2312-selling-pieces-of-wood/2312-selling-pieces-of-wood.cpp
+++ 2312-selling-pieces-of-wood/2312-selling-pieces-of-wood.cpp
@@ -9,9 +9,9 @@ public:
         
         for(int w=1;w<=m;w++){
             for(int h=1;h<=n;h++){
-                for(int a=1;a<=w;a++)
+                for(int a=1;a<=w/2;a++)
                     dp[w][h] = max(dp[w][h], dp[a][h]+dp[w-a][h]);
-                for(int a=1;a<=h;a++)
+                for(int a=1;a<=h/2;a++)
                     dp[w][h] = max(dp[w][h], dp[w][a]+dp[w][h-a]);
             }
         }
