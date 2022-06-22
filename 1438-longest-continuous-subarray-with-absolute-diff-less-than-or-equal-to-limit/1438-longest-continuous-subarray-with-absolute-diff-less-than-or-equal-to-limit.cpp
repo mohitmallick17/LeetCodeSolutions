2c2dcea6d2d42dc250b28bc8c6a9458ba8574b49
Mohit Mallick <mohitmallick17@gmail.com>
2022-06-22 16:05:21 +0530

    Time: 254 ms (39.24%), Space: 52.2 MB (71.88%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -4,16 +4,15 @@ public:
         deque<int> minQ, maxQ;
         int ans = 0;
         for(int s=0, e=0;e<nums.size();e++){
-            int num = nums[e];
-            while(!maxQ.empty() and nums[maxQ.back()] < num)maxQ.pop_back();
-            while(!minQ.empty() and nums[minQ.back()] > num)minQ.pop_back();
+            while(!maxQ.empty() and nums[maxQ.back()] < nums[e])maxQ.pop_back();
+            while(!minQ.empty() and nums[minQ.back()] > nums[e])minQ.pop_back();
             maxQ.push_back(e);
             minQ.push_back(e);
             
             while(!maxQ.empty() and !minQ.empty() and nums[maxQ.front()] - nums[minQ.front()] > limit){
+                if(minQ.front() == s)minQ.pop_front();
+                if(maxQ.front() == s)maxQ.pop_front();
                 s++;
-                if(minQ.front() < s)minQ.pop_front();
-                if(maxQ.front() < s)maxQ.pop_front();
             }
             ans = max(ans, e-s+1);
         }
