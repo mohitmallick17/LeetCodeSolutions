88541bc3709d0de90e5f067f31e45af8f44bbc36
Mohit Mallick <mohitmallick17@gmail.com>
2022-07-02 21:25:06 +0530

    Added solution - LeetHub

--- Number of ways - GFG/number-of-ways.cpp
+++ Number of ways - GFG/number-of-ways.cpp
@@ -10,12 +10,10 @@ using namespace std;
 class Solution{
 public:
     long long int arrangeTiles(int N){
+        vector<long long> dp(N+1, 1);
         
-        vector<long long>dp(max(4,N+1));
-        for(int i=0;i<4;i++)dp[i]=1;
-        for(int i=4;i<=N;i++){
-            dp[i]=dp[i-1]+dp[i-4];
-        }
+        for(int i=4;i<=N;i++)
+            dp[i] = dp[i-1] + dp[i-4];
         return dp[N];
     }
 };
