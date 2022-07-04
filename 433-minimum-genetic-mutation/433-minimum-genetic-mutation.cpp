e0450b2d6273d2d6899ae391bc60e1365a479b4f
Mohit Mallick <mohitmallick17@gmail.com>
2022-07-04 16:58:48 +0530

    Time: 5 ms (16.62%), Space: 6.7 MB (41.38%) - LeetHub

--- 433-minimum-genetic-mutation/433-minimum-genetic-mutation.cpp
+++ 433-minimum-genetic-mutation/433-minimum-genetic-mutation.cpp
@@ -1,32 +1,31 @@
 class Solution {
-    int ans = INT_MAX;
-    
-    void solve(string start, string &end, unordered_set<string> st, int steps){
-        if(steps > ans)return; // pruning
-        if(start==end){
-            ans = min(ans, steps);
-            return;
-        }
-        
-        string ss=start;
-        for(int i=0;i<8;i++){
-            char cur = start[i];
-            for(const char &nw:"ACGT"){
-                if(cur==nw)continue;
-                ss[i]=nw;
-                if(st.count(ss)){
-                    st.erase(ss);
-                    solve(ss, end, st, steps+1);
-                    st.insert(ss);
-                }
-                ss[i]=cur;
-            }
-        }
-    }
 public:
     int minMutation(string start, string end, vector<string>& bank) {
         unordered_set<string> st(bank.begin(), bank.end());
-        solve(start, end, st, 0);
-        return ans==INT_MAX?-1:ans;
+        queue<string> q;
+        q.push(start);
+        int level = 0;
+        
+        while(!q.empty()){
+            int size=q.size();
+            while(size--){
+                string cur = q.front();
+                q.pop();
+                if(cur==end)return level;
+                for(int i=0;i<8;i++){
+                    char orig = cur[i];
+                    for(const char &mod:"ACGT"){
+                        if(orig==mod)continue;
+                        cur[i]=mod;
+                        if(st.count(cur) == 0)continue; // does not exists in bank
+                        q.push(cur); // insert to queue
+                        st.erase(cur); // remove from bank(visited)
+                    }
+                    cur[i]=orig; // revert back to orig char
+                }
+            }
+            level++;
+        }
+        return -1;
     }
 };
