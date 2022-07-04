fd97e242dd5d4258151fed97040c4608d70833a7
Mohit Mallick <mohitmallick17@gmail.com>
2022-07-04 19:33:14 +0530

    Time: 511 ms (37.27%), Space: 134.7 MB (54.40%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -4,7 +4,7 @@ public:
         deque<int> dq;
         vector<int> res;
         for(int i=0;i<nums.size();i++){
-            while(!dq.empty() and dq.front() <= i-k)dq.pop_front();
+            if(!dq.empty() and dq.front() <= i-k)dq.pop_front();
             while(!dq.empty() and nums[dq.back()] < nums[i])dq.pop_back();
             dq.push_back(i);
             if(i>=k-1)
