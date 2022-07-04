82ee776e983f78be0b3c6ecdaad2231e223e37ec
Mohit Mallick <mohitmallick17@gmail.com>
2022-07-04 13:19:24 +0530

    Time: 47 ms (97.53%), Space: 17.4 MB (31.72%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,13 +1,26 @@
 class Solution {
 public:
     int minDeletions(string s) {
-          int cnt[26] = {}, res = 0;
-    unordered_set<int> used;
-    for (char ch : s)
-        ++cnt[ch - 'a'];
-    for (int i = 0; i < 26; ++i)
-        for (; cnt[i] > 0 && !used.insert(cnt[i]).second; --cnt[i])
-            ++res;
-    return res;
+        int cnt[26] = {}, res = 0;
+        for (char ch : s)
+            ++cnt[ch - 'a'];
+        priority_queue<int> pq;
+        for(int i=0;i<26;i++){
+            if(cnt[i]>0)
+                pq.push(cnt[i]);
+        }
+        unordered_set<int> vis;
+        
+        while(!pq.empty()){
+            int val = pq.top();
+            pq.pop();
+            if(vis.find(val) != vis.end()){
+                res++;
+                if(val-1 > 0)
+                    pq.push(val-1);
+            }else
+                vis.insert(val);
+        }
+        return res;
     }
 };
