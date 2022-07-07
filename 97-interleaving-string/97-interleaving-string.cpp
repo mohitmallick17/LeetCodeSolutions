6b10da91e7a17344ed95a44efc5f1336aba0ae29
Mohit Mallick <mohitmallick17@gmail.com>
2022-07-08 03:49:43 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (96.27%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
         if(s1.size() + s2.size() != s3.size())return false;
-        vector<vector<bool>> dp(s1.size()+1, vector<bool>(s2.size()+1, false));
+        vector<vector<bool>> dp(2, vector<bool>(s2.size()+1, false));
+        bool flag=true;
         
         for(int i=0;i<=s1.size();i++){
+            flag=!flag;
             for(int j=0;j<=s2.size();j++){
-                if(i==0 and j==0)dp[i][j]=true;
-                else if(i==0)dp[i][j] = dp[i][j-1] and s2[j-1]==s3[i+j-1];
-                else if(j==0)dp[i][j] = dp[i-1][j] and s1[i-1]==s3[i+j-1];
+                if(i==0 and j==0)dp[flag][j]=true;
+                else if(i==0)dp[flag][j] = dp[flag][j-1] and s2[j-1]==s3[i+j-1];
+                else if(j==0)dp[flag][j] = dp[1-flag][j] and s1[i-1]==s3[i+j-1];
                 else{
-                    dp[i][j] = (dp[i][j-1] and s2[j-1]==s3[i+j-1]) or (dp[i-1][j] and s1[i-1]==s3[i+j-1]);
+                    dp[flag][j] = (dp[flag][j-1] and s2[j-1]==s3[i+j-1]) or (dp[1-flag][j] and s1[i-1]==s3[i+j-1]);
                 }
             }
         }
-        return dp[s1.size()][s2.size()];
+        return dp[flag][s2.size()];
     }
 };
