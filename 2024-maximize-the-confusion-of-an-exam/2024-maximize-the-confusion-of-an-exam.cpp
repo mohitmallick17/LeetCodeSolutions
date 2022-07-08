0e8b01f242e82dbbd3abbd4c7365ac1040088321
Mohit Mallick <mohitmallick17@gmail.com>
2022-07-08 16:09:58 +0530

    Time: 39 ms (55.60%), Space: 10 MB (93.75%) - LeetHub

---
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    int getMax(string &str, char key, int k){
+        int res = 0;
+        int avlK=k;
+        for(int s=0, e=0, cur=0;e<str.size();e++){
+            if(str[e] == key)cur++;
+            else{
+                cur++;
+                avlK--;
+            }
+            while(avlK<0 and s<e){
+                cur--;
+                if(str[s] != key)avlK++;
+                s++;
+            }
+            res = max(res, cur);
+        }
+        return res;
+    }
+public:
+    int maxConsecutiveAnswers(string answerKey, int k) {
+        return max(getMax(answerKey, 'T', k), getMax(answerKey, 'F', k));
+    }
+};
