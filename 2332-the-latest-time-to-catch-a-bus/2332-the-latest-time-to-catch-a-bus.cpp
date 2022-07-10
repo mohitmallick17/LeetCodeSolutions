c6fe5b543f91b1663f96e7c8771d38eb1fa0dff0
Mohit Mallick <mohitmallick17@gmail.com>
2022-07-10 23:34:54 +0530

    Time: 353 ms (50.00%), Space: 66.4 MB (50.00%) - LeetHub

--- 2332-the-latest-time-to-catch-a-bus/2332-the-latest-time-to-catch-a-bus.cpp
+++ 2332-the-latest-time-to-catch-a-bus/2332-the-latest-time-to-catch-a-bus.cpp
@@ -10,11 +10,12 @@ public:
             while(j<n and passengers[j] <= buses[i] and cnt<capacity)cnt++, j++;
             
             if(i==m-1){
-                // still space left in the last bus passengers < bus*capacity
+                // still space left in the last bus (valid passengers < bus*capacity)
+                // valid passener = passenger who can get into the bus
                 if(cnt < capacity){
                     // just find the last position(time) where there is no passenger
                     int ans = buses.back();
-                    j--; // j moved out of bounds, maybe!!
+                    j--; // j moved out of bounds
                     while(j>=0 and ans == passengers[j]){
                         ans--;
                         j--;
@@ -22,10 +23,13 @@ public:
                     return ans;
                 }else{
                     // no space even in the last bus (passengers) >= bus*capacity
-                   // find the first space where we can adjust person, how? find first pair where a[i-1] != a[i]
-                    j--;
+                   // find the first space where we can adjust person, how? find first pair where a[i-1] != a[i]. basically we are kicking our next passenger out of the bus
+                    // ex [10,15] -> we kick 15 by arriving on time=14. so [10,14] catch the bus for capacity=2
+
+                    j--; // j moved out of bounds
+                    // as long as 2 passengers are contiguous, we keep sliding.
                     while(j>0 and passengers[j]-passengers[j-1]==1)j--;
-                    return passengers[j]-1;
+                    return passengers[j]-1; // kick jth passenger by arriving on passenger[j]-1 time
                 }
             }
         }
