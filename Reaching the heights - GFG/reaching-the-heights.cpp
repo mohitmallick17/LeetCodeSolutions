677e5969adab5e653b15c85e6df222a18770639e
Mohit Mallick <mohitmallick17@gmail.com>
2022-07-12 00:02:14 +0530

    Added solution - LeetHub

--- Reaching the heights - GFG/reaching-the-heights.cpp
+++ Reaching the heights - GFG/reaching-the-heights.cpp
@@ -36,48 +36,19 @@ int main() {
 
 vector<int> reaching_height(int n, int a[]) {
     // Complete the function
-    // sort(a, a+1);
-    // vector<int> pos(n);
-    // int st=0, en=n-1;
-    // int sum=0;
-    // int idx = 0;
-    // while(st < en){
-    //     pos[idx++] = a[en];
-    //     pos[idx++] = a[st];
-    //     sum += a[en--] + a[st++];
-        
-    // }
-    // if(st==en){
-    //     if(st&1)
-    //         pos[idx] = a[st], sum-=a[st];
-    //     else
-    //         pos[idx] = a[en], sum+=a[en];
-    // }
-    // return sum==0 ? vector<int>{-1}:pos;
-
-    sort(a , a+n) ;         //start ke N/2 elements are minimum and after that N/2 elements are maximum
-   vector<int> ans(n) ; 
-   
-   int sum = 0 ;                   //To check kya last me ham Ground floor par hi wapas aa gye 
-   
-   int maxEle = n-1 ;              //maximum elements INDEX 
-   for(int i=0; i<n ; i+=2)        //FILL maximum element at ODD index                        
-   {               
-       ans[i] = a[maxEle] ; 
-       maxEle-- ; 
-       sum += ans[i] ; 
-   }
-   
-   int minEle = 0 ;                //maximum elements INDEX 
-   for(int i=1 ; i<n ; i+=2)
-   {
-       ans[i] = a[minEle] ; 
-       minEle++ ; 
-       sum -= ans[i] ; 
-   }
-   
-   if(sum == 0)return {-1} ; 
-   
-   return ans ;   
+    sort(a, a+n);
+    vector<int> pos(n);
+    int st=0, en=n-1;
+    int sum=0;
+    int idx = 0;
+    while(st < en){
+        pos[idx++] = a[en];
+        pos[idx++] = a[st];
+        sum += a[en] - a[st];
+        en--, st++;
+    }
+    if(st==en){
+        pos[idx] = a[st], sum-=a[st];
+    }
+    return sum==0 ? vector<int>{-1}:pos;
 }
-
