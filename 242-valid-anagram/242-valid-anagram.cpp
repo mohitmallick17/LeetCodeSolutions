ef28a98514a9980950afc42ac941777f1f49d2dd
Mohit Mallick <mohitmallick17@gmail.com>
2022-07-28 21:17:12 +0530

    Time: 9 ms (78.39%), Space: 7.3 MB (45.59%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size())return false;
+        vector<int> cnt(26, 0);
+        for(int i=0;i<s.size();i++){
+            cnt[s[i]-'a']++;
+            cnt[t[i]-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(cnt[i])return false;
+        }
+        return true;
+    }
+};
