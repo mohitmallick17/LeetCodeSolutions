ec46251a0143e0ac63795b8a7084ba152650ec17
Mohit Mallick <mohitmallick17@gmail.com>
2022-07-30 04:03:36 +0530

    Time: 328 ms (90.07%), Space: 86.7 MB (78.93%) - LeetHub

--- 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.cpp
+++ 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.cpp
@@ -7,8 +7,8 @@ public:
         }
         long long ans=0;
         
-        for(int i=1;i<32;i++){
-            for(int j=1;j<32;j++){
+        for(int i=1;i<30;i++){
+            for(int j=1;j<30;j++){
                 if(i+j >= k)
                     ans += cnt[i]*cnt[j];
             }
