74a31ea7c002bf1ccac8ccb903c0873d23f84c37
Mohit Mallick <mohitmallick17@gmail.com>
2022-08-07 18:02:26 +0530

    Time: 202 ms (94.98%), Space: 77.7 MB (86.22%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -4,15 +4,19 @@ public:
         unordered_map<int, int> mp;
         int n=arr.size();
         for(int &a:arr)mp[a]++;
-        priority_queue<int> pq;
-        for(auto &x:mp)pq.push(x.second);
         
-        int i=0;
+        vector<int> freq_arr(n+1, 0);
+        for(auto &x:mp){
+            freq_arr[x.second]++; // increase count of freqency K
+        }
+        int len=0;
         int cnt=0;
-        while(!pq.empty() and i < (n+1)/2){
-            i += pq.top();
-            pq.pop();
-            cnt++;
+        for(int i=freq_arr.size()-1;i>0 and len < n/2;i--){
+            while(freq_arr[i] > 0  and len < n/2){
+                len+=i;
+                freq_arr[i]--;
+                cnt++;
+            }
         }
         return cnt;
     }
