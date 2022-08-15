5068e5e2b9b6772429988d369ed934be26a4f420
Mohit Mallick <mohitmallick17@gmail.com>
2022-08-15 17:36:40 +0530

    Time: 59 ms (92.61%), Space: 23.5 MB (34.46%) - LeetHub

---
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        unordered_map<string, int> mp;
+        
+        for(int i=0;i+10<=s.size();i++){
+            mp[s.substr(i, 10)]++;
+        }
+        vector<string> res;
+        for(auto &[k,v]:mp){
+            if(v > 1)res.push_back(k);
+        }
+        return res;
+    }
+};
