634c9d9f94b3b3e81747d9f132e97b6063b5a1dd
Mohit Mallick <mohitmallick17@gmail.com>
2022-08-21 09:55:15 +0530

    Time: 28 ms (100.00%), Space: 14.5 MB (100.00%) - LeetHub

--- 2384-largest-palindromic-number/2384-largest-palindromic-number.cpp
+++ 2384-largest-palindromic-number/2384-largest-palindromic-number.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     string largestPalindromic(string num) {
-        if(num.size()==1)return num;
+        vector<int> cnt(10, 0);
         
-        vector<int> mp(10, 0);
-        
-        for(char &c:num)mp[c-'0']++;
+        for(char &c:num)cnt[c-'0']++;
         
         string f, r;
         
         for(int i=9;i>=0;i--){
-            int x = mp[i]/2;
+            int x = cnt[i]/2;
             f += string(x, '0'+i);
-            mp[i] -= 2*x;
+            cnt[i] -= 2*x;
         }
         if(f.front()=='0')f.clear();
         bool isOdd=false;
         char mid;
         for(int i=9;i>=0;i--){
-            if(mp[i] > 0){
+            if(cnt[i] > 0){
                 isOdd=true;
                 mid='0'+i;
                 break;
