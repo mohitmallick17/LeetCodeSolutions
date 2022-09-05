4dcbbba418cb413333605ba20e76c7a7faf9a6ea
Mohit Mallick <mohitmallick17@gmail.com>
2022-09-06 01:58:00 +0530

    Time: 26 ms (32.63%), Space: 9.5 MB (33.74%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,27 +1,20 @@
 class Solution {
-    int dfs(int i, int j, vector<vector<int>>& grid){
-        if(i<0 or j<0 or i>=grid.size() or j>=grid[i].size() or grid[i][j]==1)return 0; // out of bound / water
-        grid[i][j] = 1; // mark it as water (visited)
+    bool dfs(int i, int j, vector<vector<int>>& grid){
+        if(i < 0 or j < 0 or i==grid.size() or j==grid[i].size() or grid[i][j]==1)return true;
+        bool isBoundary = !(i==0 or j==0 or i==grid.size()-1 or j==grid[i].size()-1);
+        grid[i][j]=1;
+        bool a = dfs(i+1, j, grid), b=dfs(i-1, j, grid), c = dfs(i, j+1, grid), d = dfs(i, j-1, grid);
         
-        return 1 + dfs(i+1, j, grid) + dfs(i, j+1, grid) + dfs(i-1, j, grid) + dfs(i, j-1, grid);
+        return isBoundary && a && b && c && d;
     }
 public:
     int closedIsland(vector<vector<int>>& grid) {
-        int res = 0;
-        
+        int ans=0;
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[i].size();j++){
-                if(i==0 or j==0 or i==grid.size()-1 or j==grid[i].size()-1)
-                    dfs(i, j, grid);
+                if(grid[i][j]==0)ans += dfs(i,j,grid);
             }
         }
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[i].size();j++){
-                res += (dfs(i, j, grid) > 0 ? 1 : 0);
-            }
-        }
-        
-        return res;
-        
+        return ans;
     }
 };
