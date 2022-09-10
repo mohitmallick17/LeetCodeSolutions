4b064488eb0c6ba2b60643a0a91bc9177fc8cc09
Mohit Mallick <mohitmallick17@gmail.com>
2022-09-11 03:20:46 +0530

    Time: 54 ms (16.24%), Space: 21.3 MB (6.55%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -15,9 +15,29 @@ class Solution {
         return dp[idx][k][canBuy]=ans;
     }
 public:
-    int maxProfit(int k, vector<int>& prices) {
+    int maxProfit(int kk, vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(k+1, vector<int>(2, -1)));
-        return solve(0,1,k,prices,dp);    
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(kk+1, vector<int>(2, 0)));
+        
+        for(int idx=n-1;idx>=0;idx--){
+            for(int k=1;k<=kk;k++){
+                for(int canBuy=0;canBuy<=1;canBuy++){
+                    // cout << idx << ' ' << k << ' ' << canBuy << '\n';
+                    int ans;
+                    if(canBuy){
+                        int buy = -prices[idx] + dp[idx+1][k][0];
+                        int notBuy = dp[idx+1][k][1];
+                        ans = max(buy, notBuy);
+                    }else{
+                        int sell = prices[idx] + dp[idx+1][k-1][1];
+                        int notSell = dp[idx+1][k][0];
+                        ans = max(sell, notSell);
+                    }
+                    dp[idx][k][canBuy]=ans;
+                }
+            }
+        }
+        
+        return dp[0][kk][1];  
     }
 };
