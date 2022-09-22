393fc1577138cb7c18df25fe823921fcb2f3ac02
Mohit Mallick <mohitmallick17@gmail.com>
2022-09-22 13:34:38 +0530

    Time: 117 ms (90.02%), Space: 46.1 MB (66.34%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
@@ -1,40 +1,22 @@
 class Solution {
 public:
     vector<int> sumEvenAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
-        vector<int> ans;
-        int sum=0;
-        int n = queries.size();
-        for(auto num:nums){
-            if(num%2==0) sum+=num;
+        vector<int> res;
+        int sum = 0;
+        for(auto &x:nums){
+            if(x % 2 == 0)sum += x;
         }
         
-        for(int i=0;i<n;i++){
-            int val = queries[i][0], ind = queries[i][1];
-            int oldVal = nums[ind];
-            nums[ind]+=val;
-            
-            bool wasEven = ( oldVal%2 == 0 );
-            bool nowEven = ( nums[ind]%2 == 0 );
-            
-            if(wasEven and nowEven ){
-                sum+=val;
-                ans.push_back(sum);
+        for(auto &q:queries){
+            int idx = q[1], val=q[0];
+            if(nums[idx] % 2 == 0)
+                sum -= nums[idx];
+            nums[idx]+=val;
+            if(nums[idx] % 2 == 0){
+                sum += nums[idx];
             }
-            else if(wasEven and !nowEven){
-                sum-= oldVal;
-                ans.push_back(sum);
-            }
-            else if(!wasEven and nowEven){
-                sum+=nums[ind];
-                ans.push_back(sum);
-            }
-            else{
-                ans.push_back(sum);
-            }
+            res.push_back(sum);
         }
-        
-        return ans;
-        
-        
+        return res;
     }
 };
