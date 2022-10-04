7ac377429b9a827fd69f2013394597aaffe877b8
Mohit Mallick <mohitmallick17@gmail.com>
2022-10-04 16:49:03 +0530

    Time: 970 ms (28.65%), Space: 176.4 MB (19.99%) - LeetHub

---
+++ 2424-longest-uploaded-prefix/2424-longest-uploaded-prefix.cpp
@@ -0,0 +1,35 @@
+class LUPrefix {
+    set<int> s;
+    int size;
+public:
+    LUPrefix(int n) {
+        size=n;
+        // insert all unprocessed videos, initially all (1 to n)
+        for(int i=1;i<=n;i++)
+            s.insert(i);
+    }
+    
+    void upload(int video) {
+        // delete the uploaded video from set
+        s.erase(video);
+    }
+    
+    int longest() {
+        // now, if set is empty, it means all videos are uploaded. hence answer = size
+        if(s.empty())
+            return size;
+        else{
+            // otherwise, ans = the minimum element in set minus 1
+            // this is because all elements lesser than it are already processed
+            int f = *s.begin();
+            return f-1;
+        }
+    }
+};
+
+/**
+ * Your LUPrefix object will be instantiated and called as such:
+ * LUPrefix* obj = new LUPrefix(n);
+ * obj->upload(video);
+ * int param_2 = obj->longest();
+ */
