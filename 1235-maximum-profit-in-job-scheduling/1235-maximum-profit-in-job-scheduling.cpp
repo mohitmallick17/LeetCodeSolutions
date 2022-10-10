c20263609dca22fffcf847ccbc48aff368adbe18
Mohit Mallick <mohitmallick17@gmail.com>
2022-10-11 02:22:45 +0530

    Time: 428 ms (23.01%), Space: 63.9 MB (30.31%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,38 +1,30 @@
-struct item{
-    int st;
-    int et;
-    int p;
-    item(int s, int e , int pp) {
-        st=s;
-        et=e;
-        p=pp;
-    }
-    bool operator<(item &b){
-        return et < b.et;
-    }
-};
 class Solution {
 public:
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
-        int n=startTime.size();
-        vector<item> v;
-        
-        for(int i=0;i<n;i++){
-            v.push_back(item(startTime[i], endTime[i], profit[i]));
-        }
+        int n=profit.size();
+        vector<vector<int>> v(n);
+        for(int i=0;i<n;i++)
+            v[i] = {endTime[i], startTime[i], profit[i]};
         sort(v.begin(), v.end());
-        map<int, int> mp; // key: endTime, val: profit
-        mp[0] = 0;
+        map<int,int> mp; // end - profit: until point end, max profit I can make
+        mp[0]=0;
         
         for(int i=0;i<n;i++){
-            auto it = mp.upper_bound(v[i].st);
-            it--;
-            int val = v[i].p + it->second;
-            if(val > mp.rbegin()->second){
-                mp[v[i].et] = val;
+            int s=v[i][1], e=v[i][0], p=v[i][2];
+            int pick=0;
+            int not_pick = mp.rbegin()->second;
+            if(mp.rbegin()->first <= s){
+                // extend the prev job
+                pick = p + mp.rbegin()->second;
+            }else{
+                // can't extend the prev job, so binary search the last possible job
+                auto it = mp.upper_bound(s); // find someone > start
+                it--; // decrease it, will point to someone <= start
+                pick = p + it->second;
             }
+            mp[e] = max(pick, not_pick);
         }
-        
+        // for(auto &x:mp)cout << x.first << ' ' << x.second << '\n';
         return mp.rbegin()->second;
     }
 };
